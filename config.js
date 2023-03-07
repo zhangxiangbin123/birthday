e1380d8f6ad43ba427a9f2ea14d72af86d69f51f
zhangxiangbin123 <109203746+zhangxiangbin123@users.noreply.github.com>
2023-03-07 10:57:47 +0800

    Create config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 张祥斌 2023年03月07日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
